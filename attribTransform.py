53a00598045ba7daa7e0c6a223aaa804f5dc1ff8
sumitranandan-k <72206230+sumitranandan-k@users.noreply.github.com>
2023-09-27 22:34:26 +0200

    Add files via upload

--- attribTransform.py
+++ attribTransform.py
@@ -1,19 +1,24 @@
 import pandas as pd
 
+#read csv
 df = pd.read_csv('C:\Temp\src.csv')
 
+#get all column names
 attribs = df.columns.tolist()
-
+#remove the asset id from column list, only attribute names remain
 attribs.remove('AssetID')
 
+#empty dictionary
 data = []
 
+#construct ductionary for attributes that contain values per assetId
 for ind in df.index:
     for attrib in attribs:
         if not pd.isnull(df[attrib][ind]):
             data.append({'AssetId': df['AssetID'][ind], 'Attribute Name': attrib, 'Attribute value':df[attrib][ind]})
 
-
+#convert dictionary to dataframe
 result = pd.DataFrame(data)
 
+#convert dataframe to csv
 result.to_csv('C:\Temp\out.csv',index=False)
